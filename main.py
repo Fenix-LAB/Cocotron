4d38d90193abc192360380e2e1ac15a3f9201dde
Christian-RH <fenixcr.123@gmail.com>
2022-09-13 08:34:10 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,92 @@
+import numpy
+from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
+from PyQt5.QtCore import *
+from gui_design import *
+from PyQt5.QtGui import *
+
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
+        self.setupUi(self)
+
+        # Usamos la funcion QPoint() para guardar la posicion del mouse
+        self.click_position = QPoint()
+        # Se configura la ventana
+        self.btn_normal.hide()
+        self.btn_min.clicked.connect(lambda: self.showMinimized())
+        self.btn_cerrar.clicked.connect(self.control_btn_cerrar)
+        self.btn_normal.clicked.connect(self.control_btn_normal)
+        self.btn_max.clicked.connect(self.control_btn_maximizar)
+        self.btn_menu.clicked.connect(self.mover_menu)
+
+        # Se elimina la barra de titulo por default
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setWindowOpacity(1)
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        # Size grip
+        self.gripSize = 10
+        self.grip = QtWidgets.QSizeGrip(self)
+        self.grip.resize(self.gripSize, self.gripSize)
+
+        # Movimiento de la ventana
+        self.frame_superior.mouseMoveEvent = self.mover_ventana
+
+    def mover_menu(self):
+        if True:
+            width = self.frame_menu.width()
+            normal = 0
+            if width == 0:
+                extender = 250
+            else:
+                extender = normal
+            self.animacion = QPropertyAnimation(self.frame_menu, b'minimumWidth')
+            self.animacion.setDuration(300)
+            self.animacion.setStartValue(width)
+            self.animacion.setEndValue(extender)
+            self.animacion.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+            self.animacion.start()
+
+    def control_btn_cerrar(self):
+        self.close()
+        #cap.release()
+        self.label.clear()
+
+    def control_btn_normal(self):
+        self.showNormal()
+        self.btn_normal.hide()
+        self.btn_max.show()
+
+    def control_btn_maximizar(self):
+        self.showMaximized()
+        self.btn_max.hide()
+        self.btn_normal.show()
+
+    def resizeEvent(self, event):
+        rect = self.rect()
+        self.grip.move(rect.right() - self.gripSize, rect.bottom() - self.gripSize)
+
+    def mousePressEvent(self, event):
+        self.click_posicion = event.globalPos()
+
+    def mover_ventana(self, event):
+        if self.isMaximized() == False:
+            if event.buttons() == QtCore.Qt.LeftButton:
+                self.move(self.pos() + event.globalPos() - self.click_posicion)
+                self.click_posicion = event.globalPos()
+                event.accept()
+        if event.globalPos().y() <= 5 or event.globalPos().x() <= 5:
+            self.showMaximized()
+            self.btn_max.hide()
+            self.btn_normal.show()
+        else:
+            self.showNormal()
+            self.btn_normal.hide()
+            self.btn_max.show()
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = MyApp()
+    window.show()
+    app.exec_()
